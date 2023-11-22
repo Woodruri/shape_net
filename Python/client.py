4cae98ceea30032acc1e32b4543d0c2b7dbb3cc6
Imane Jahid <imane2@pdx.edu>
2023-11-22 15:50:56 -0800

    Started on the client side part of the application

--- Python/client.py
+++ Python/client.py
@@ -3,18 +3,26 @@ import tkinter as tk
 from shapes import shape
 import json as js
 
-host = "127.0.0.1"
-port = 5050
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
 
 
 
-def recieve():
-    while True:
-        try:
-            message = client.recv(1024)
-        
-        except:
-            pass
+
+def clientHost():
+
+    serverHost= "127.0.0.1"
+    serverPort = 5050
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((serverHost, serverHost))
+    client.sendData()
+    
+
+
+
+
+
+def sendData():
+    pass
+
+
+
