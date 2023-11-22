323b61571ab3ab79943eb24411bc230f36eaed61
Riley <woodruff.l.riley@gmail.com>
2023-11-22 15:14:20 -0800

    started on client.py

--- Python/client.py
+++ Python/client.py
@@ -1,6 +1,20 @@
-import socket as sock
+import socket
 import tkinter as tk
 from shapes import shape
 import json as js
 
+host = "127.0.0.1"
+port = 5050
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+
+
+def recieve():
+    while True:
+        try:
+            message = client.recv(1024)
+        
+        except:
+            pass
